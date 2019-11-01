c680e774aa9d91a55f92a57b33b021ce1bd319f0
RH <52927295@qq.com>
2019-11-02 00:57:08 +0800

    wmsActivity

--- app/src/main/java/com/example/osmdroid/wms/WmsActivity.java
+++ app/src/main/java/com/example/osmdroid/wms/WmsActivity.java
@@ -9,19 +9,35 @@ import com.example.osmdroid.R;
 import org.osmdroid.views.MapView;
 import org.osmdroid.wms.WMSTileSource;
 
+/**
+ * 私有发布wms
+ * boyang_wms 替换成自己的的URL
+ */
 public class WmsActivity extends AppCompatActivity {
     MapView map;
-   final static String[] beyonserver = {"http://180.167.113.60:9090/geoserver/beyondb/wms?"};//和田190基图
-    WMSTileSource  wmsTileSource = new WMSTileSource("beyonserver", beyonserver, "beyondb%3Aimg_glob_0_7", "1.1.1", "EPSG:4326", "", 256);//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wms);
         map = (MapView) findViewById(R.id.map);
-        map.setTileSource(wmsTileSource);//基础瓦片图
+        map.setTileSource(getWMSTileSource());//基础瓦片图
     }
-    public void onResume(){
+
+    private WMSTileSource getWMSTileSource() {
+        final String[] beyonserver = {getResources().getString(R.string.boyang_wms)};//和田190基图
+        WMSTileSource wmsTileSource = new WMSTileSource(
+                "beyonserver",
+                beyonserver,
+                "beyondb%3Aimg_glob_0_7",
+                "1.1.1",
+                "EPSG:4326",
+                "",
+                256);//
+        return wmsTileSource;
+    }
+
+    public void onResume() {
         super.onResume();
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -29,7 +45,8 @@ public class WmsActivity extends AppCompatActivity {
         //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
-    public void onPause(){
+
+    public void onPause() {
         super.onPause();
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
